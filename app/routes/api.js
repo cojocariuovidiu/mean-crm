881db161af6c873d4492defb3bfe24279c9d53ac
GJ Bogaerts <gj@raker.nl>
2016-01-02 13:57:07 +0100

    User authentication added

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,10 +4,66 @@ var User = require('../models/user');
 var apiRouter = express.Router();
 var superSecret = 'JamesonsIsMyFavoriteDrink';
 
+apiRouter.post('/authenticate', function (req, res) {
+    User.findOne({
+        userName: req.body.userName
+    }).select('name userName password').exec(function (err, user) {
+        if (err) throw err;
+        if (!user) {
+            res.json({
+                success: false,
+                message: 'Authentication failed. User not found.'
+            });
+        } else if (user) {
+            var validPassword = user.comparePassword(req.body.password);
+            if (!validPassword) {
+                res.json({
+                    success: false,
+                    message: 'Authentication failed. Wrong password.'
+                });
+            } else {
+                var token = jwt.sign({
+                    name: user.name,
+                    userName: user.userName,
+
+                }, superSecret, {
+                    expiresInMinutes: 1440
+                });
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token',
+                    token: token
+                })
+            }
+        }
+    });
+
+});
+
 apiRouter.use(function (req, res, next) {
     console.log('Somebody just came to our app');
-    //authentication will happen here
-    next();
+
+    var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+    if (token) {
+        jwt.verify(token, superSecret, function (err, decoded) {
+            if (err) {
+                return res.status(403).send({
+                    success: false,
+                    message: 'Failed to authenticate token'
+                });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        })
+    } else {
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided'
+        });
+    }
+
+    //    next();
 });
 
 
@@ -72,6 +128,9 @@ apiRouter.route('/users/:user_id').get(function (req, res) {
             message: 'User successfully deleted!'
         });
     })
+});
+apiRouter.get('/me', function (req, res) {
+    res.send(req.decoded);
 });
 
 module.exports = apiRouter;
